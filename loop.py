8622131312654710aa5cfc827e775e18c089c05d
t99forever <ybezema@gmail.com>
2025-02-15 23:02:12 +0100

    Loop in function geplaatst

--- loop.py
+++ loop.py
@@ -2,9 +2,12 @@ import os
 
 oorsprongMap = input()
 
-for roots, dirs, files in os.walk(oorsprongMap):
-    bestandMap = roots
-    for file in files:
-        if file.endswith(".flac") == True or file.endswith(".mp3") == True:
-            bestandNaam = str(bestandMap + "\\" + file)
-            print(bestandNaam)
+def getBestandNaam(oorsprongMap):
+    for roots, dirs, files in os.walk(oorsprongMap):
+        bestandMap = roots
+        for file in files:
+            if file.endswith(".flac") == True or file.endswith(".mp3") == True:
+                bestandNaam = str(bestandMap + "\\" + file)
+                print(bestandNaam)
+
+getBestandNaam(oorsprongMap)
