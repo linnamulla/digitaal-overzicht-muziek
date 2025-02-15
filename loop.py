53ed823c547743d0b5c04d4b60eb52401009d6cd
t99forever <ybezema@gmail.com>
2025-02-15 22:56:52 +0100

    Berekening variabele bestandNaam gecorrigeerd

--- loop.py
+++ loop.py
@@ -1,9 +1,10 @@
 import os
 
-mapNaam = input()
+oorsprongMap = input()
 
-for roots, dirs, files in os.walk(mapNaam):
+for roots, dirs, files in os.walk(oorsprongMap):
+    bestandMap = roots
     for file in files:
         if file.endswith(".flac") == True or file.endswith(".mp3") == True:
-            bestandNaam = str(mapNaam + "\\\\" + file)
+            bestandNaam = str(bestandMap + "\\" + file)
             print(bestandNaam)
