b929dd0497456b658be3079a72c71bbf8be63964
t99forever <ybezema@gmail.com>
2025-02-16 00:11:19 +0100

    Pseudocode toegevoegd voor ophalen metadata

--- loop.py
+++ loop.py
@@ -2,9 +2,17 @@ import os
 
 oorsprongMap = input()
 
+def fooPseudo(bestandNaam):
+    artiestLijst = []; albumartiestLijst = []
+    #artiest = bestandNaam.artist(); albumartiest = bestandNaam.albumartist(); album = bestandNaam.album()
+    #artiestLijst = artiestLijst.append(artiest)
+    #albumartiestLijst = albumartiestLijst.append(albumartiest)
+    print(bestandNaam)
+    return artiestLijst, albumartiestLijst
+
 for roots, dirs, files in os.walk(oorsprongMap):
     bestandMap = roots
     for file in files:
         if file.endswith(".flac") == True or file.endswith(".mp3") == True:
             bestandNaam = str(bestandMap + "\\" + file)
-            print(bestandNaam)
+            fooPseudo(bestandNaam)
