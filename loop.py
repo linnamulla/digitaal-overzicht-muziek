3129f7c9801331a015ae4c417de2d64e3abd628b
t99forever <ybezema@gmail.com>
2025-02-15 23:02:53 +0100

    Function verwijderd

--- loop.py
+++ loop.py
@@ -2,12 +2,9 @@ import os
 
 oorsprongMap = input()
 
-def getBestandNaam(oorsprongMap):
-    for roots, dirs, files in os.walk(oorsprongMap):
-        bestandMap = roots
-        for file in files:
-            if file.endswith(".flac") == True or file.endswith(".mp3") == True:
-                bestandNaam = str(bestandMap + "\\" + file)
-                print(bestandNaam)
-
-getBestandNaam(oorsprongMap)
+for roots, dirs, files in os.walk(oorsprongMap):
+    bestandMap = roots
+    for file in files:
+        if file.endswith(".flac") == True or file.endswith(".mp3") == True:
+            bestandNaam = str(bestandMap + "\\" + file)
+            print(bestandNaam)
